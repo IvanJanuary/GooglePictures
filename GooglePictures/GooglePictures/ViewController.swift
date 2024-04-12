263361c27c391a3758da16d9c16eb8e5634f91d3
Ivan <>
2024-04-12 15:44:57 +0300

    Create GooglePictures project

---
+++ GooglePictures/GooglePictures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GooglePictures
+//
+//  Created by Ivan on 12.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
