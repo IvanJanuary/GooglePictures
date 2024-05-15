0319b35c90a248712b98a152af5e7a115b575490
Ivan <>
2024-05-15 16:49:47 +0300

    Renew IBAction func downloadButtonTapped

--- GooglePictures/GooglePictures/Controller/DetailViewController.swift
+++ GooglePictures/GooglePictures/Controller/DetailViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    var apiHelper = ApiHelper()
+    var picture: UIImage?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.imageView.image = picture
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func downloadButtonTapped(_ sender: UIButton) {
+        if let image = imageView.image {
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
     }
-    */
-
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            print("Error saving image: \(error.localizedDescription)")
+        } else {
+            print("Image saved successfully")
+        }
+    }
 }
