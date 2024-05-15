31b9f8eff6f36c31bc596d9e807ae3daf1a4bc9b
Ivan <>
2024-05-15 16:56:38 +0300

    Create Search Bar Create Google request in ViewController Scroll forward one page Create Activity Indicator and activityIndicatoConstraints func

--- GooglePictures/GooglePictures/Controller/ViewController.swift
+++ GooglePictures/GooglePictures/Controller/ViewController.swift
@@ -7,48 +7,148 @@
 
 import UIKit
 
-class ViewController: UIViewController, UISearchResultsUpdating {
+class ViewController: UIViewController, UISearchBarDelegate {
+
+    let searchBar = UISearchBar()
+    var activityIndicator = UIActivityIndicatorView()
+    var searchText: String = ""
+    var page = 1
     
-    let idCell = "picCell"
-    let searchController = UISearchController(searchResultsController: nil)
-    var itemCollectionArray = ["AtlanticPuffin.jpg", "DrinkingJaguar", "GreenTurtle.jpg"]
+    var items: [UIImage] = []
     
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Search"
-        searchController.searchResultsUpdater = self
-        navigationItem.searchController = searchController
+        searchBar.delegate = self
+        view.addSubview(searchBar)
         
         collectionView.dataSource = self
         collectionView.delegate = self
+        
+        activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 25, height: 25)
+        self.view.addSubview(activityIndicator)
+        
+        activityIndicatorConstraint()
+    }
+    
+    func activityIndicatorConstraint() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        activityIndicator.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        activityIndicator.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
     }
 
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else {
-            return
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchBar.frame = CGRect(x: 10, y: view.safeAreaInsets.top, width: view.frame.size.width-20, height: 50)
+    }
+    
+    // -----
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        guard let searchBarText = searchBar.text else { return }
+        page = 1
+        searchText = searchBarText
+        items = []
+        collectionView.reloadData()
+        
+        fetchPicture(withQuery: searchBarText)
+    }
+    
+    
+    // ------
+    func fetchPicture(withQuery query: String) {
+        activityIndicator.startAnimating()
+        
+        let startElementOnPage = (page * 10) + 1
+        guard var url = URL(string: "https://www.googleapis.com/customsearch/v1") else { return }
+        url.append(queryItems: [URLQueryItem(name: "key", value: "AIzaSyD-ZkNR3zkwYhY4uK2EdoFXnJbfCZLIzXA"),
+                                URLQueryItem(name: "cx", value: "c6f64a506feec48ec"),
+                                URLQueryItem(name: "searchType", value: "image"),
+                                URLQueryItem(name: "q", value: query),
+                                URLQueryItem(name: "start", value: "\(startElementOnPage)")
+                               ])
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "Unknown error")
+                DispatchQueue.main.async {
+                    if let urlError = error as? URLError, urlError.code == .notConnectedToInternet {
+                        self?.noInternetAlert()
+                    } else {
+                            self?.fetchPicture(withQuery: query)
+                    }
+                }
+                return
+            }
+            
+            do {
+                let jsonResult = try JSONDecoder().decode(GetPicture.self, from: data)
+                jsonResult.items.forEach { link in
+                    guard let pictureUrl = URL(string: link.link) else { return }
+                    URLSession.shared.dataTask(with: pictureUrl) { [weak self] data, _, error in
+                        guard let data = data, error == nil, let image = UIImage(data: data) else { return }
+                        DispatchQueue.main.async {
+                            self?.items.append(image)
+                            self?.collectionView.reloadData()
+                        }
+                    }.resume()
+                }
+                
+                DispatchQueue.main.async {
+                    self?.activityIndicator.stopAnimating()
+                }
+            }
+            catch {
+                DispatchQueue.main.async {
+                    self?.errorAlert(with: error, completion: { [weak self] in
+                        self?.fetchPicture(withQuery: query)
+                    })
+                }
+            }
         }
 
-        print(text)
+        task.resume()
     }
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return itemCollectionArray.count
+        return items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let itemCell = collectionView.dequeueReusableCell(withReuseIdentifier: "picCell", for: indexPath) as? ItemCollectionViewCell else {
-            
-            itemCell = itemCollectionArray[indexPath.row]
-            return itemCell
-        }
-        return UICollectionViewCell()
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCollectionViewCell", for: indexPath) as? ItemCollectionViewCell else { return UICollectionViewCell() }
+        
+        let imageUrlString = items[indexPath.item]
+        cell.configure(with: imageUrlString)
+        
+        if (indexPath.item + 1) % 10 == 0 && indexPath.item == items.count - 1 {
+        page += 1
+        fetchPicture(withQuery: searchText)
+            }
+        
+        return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 170 ,height: 130)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedPicture = items[indexPath.item]
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        guard let destination = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else {
+            return
+        }
+        destination.picture = selectedPicture
+        navigationController?.pushViewController(destination, animated: true)
+    }
 }
-
