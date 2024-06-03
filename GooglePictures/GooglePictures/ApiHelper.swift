b0bb13acd2e54d0ce7315633f3ec7805deb996b7
Ivan <>
2024-06-03 16:33:59 +0300

    Add makeUrlRequest func to ApiHelper struct

--- GooglePictures/GooglePictures/ApiHelper.swift
+++ GooglePictures/GooglePictures/ApiHelper.swift
@@ -9,6 +9,26 @@ import Foundation
 
 struct ApiHelper {
     
+    func makeUrlRequest(url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Request error: \(error.localizedDescription)")
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No Data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            completion(.success(data))
+        }
+        
+        task.resume()
+    }
+    
     func makePictureRequest(imageLink: String, completion: @escaping (Result<Data, Error>) -> Void) {
         guard let url = URL(string: imageLink) else {
             completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
@@ -16,7 +36,7 @@ struct ApiHelper {
                        
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
-                print("Gender request error: \(error.localizedDescription)")
+                print("Request error: \(error.localizedDescription)")
                 completion(.failure(error))
                 return
         }
